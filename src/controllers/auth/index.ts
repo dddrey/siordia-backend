69083f4ae6011045f48c8b297a192894d7546789
marktcheltsov <104628734+marktcheltsov@users.noreply.github.com>
2025-02-25 13:02:00 +0300

    beta 1.1

---
+++ src/controllers/auth/index.ts
@@ -0,0 +1,10 @@
+export * from "./authenticate";
+
+// Базовый пример контроллера аутентификации
+export const login = async (req: any, res: any) => {
+  // Логика входа
+};
+
+export const register = async (req: any, res: any) => {
+  // Логика регистрации
+};
