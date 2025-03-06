4b970005de27742c94c76563068d281fab79a4bd
drunya <drachyov.andrew@gmail.com>
2025-03-06 20:50:13 +0300

    bind to 0.0.0.0

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = Number(process.env.PORT) || 4000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
