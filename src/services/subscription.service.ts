32ec689cee222adb59ed4a44484eacaea8eaacaf
marktcheltsov <104628734+marktcheltsov@users.noreply.github.com>
2025-03-23 16:49:17 +0300

    Update subscription.service.ts

--- src/services/subscription.service.ts
+++ src/services/subscription.service.ts
@@ -20,51 +20,13 @@ export const createOrUpdateSubscription = async (
       `Attempting to handle subscription for user ${userId}, type: ${type}`
     );
 
-    // Ищем существующую подписку
-    const existingSubscription = await prisma.subscription.findFirst({
+    const existingSubscription = await prisma.subscription.deleteMany({
       where: {
         userId,
-        type,
-        active: true,
       },
     });
 
-    console.log("Existing subscription:", existingSubscription);
-
-    const now = new Date();
-
-    if (existingSubscription) {
-      console.log("Subscription active, extending from current end date");
-
-      const newEndDate = addMonths(now, 1);
-
-      console.log("New endDate will be:", newEndDate);
-
-      const updatedSubscription = await prisma.subscription.update({
-        where: { id: existingSubscription.id },
-        data: {
-          endDate: newEndDate,
-          active: true,
-        },
-      });
-
-      console.log("Updated subscription:", updatedSubscription);
-      return updatedSubscription;
-    }
-
-    // Если подписки нет, создаем новую
-    console.log("Creating new subscription from:", now);
-    const newSubscription = await prisma.subscription.create({
-      data: {
-        userId,
-        type,
-        endDate: addMonths(now, 1),
-        active: true,
-      },
-    });
-
-    console.log("Created new subscription:", newSubscription);
-    return newSubscription;
+    console.log("Existing subscription deleted:", existingSubscription);
   } catch (error) {
     console.error("Error in createOrUpdateSubscription:", error);
     throw error;
