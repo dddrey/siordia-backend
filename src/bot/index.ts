e707bc3a0379cbd9a94c237dcef769dac1c772e4
marktcheltsov <104628734+marktcheltsov@users.noreply.github.com>
2025-03-23 13:56:08 +0300

    Update index.ts

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -13,9 +13,12 @@ bot.on("pre_checkout_query", async (ctx) => {
 bot.on(":successful_payment", async (ctx) => {
   ctx.reply("Вы получили Pro версию");
   console.log("successful_payment: ", ctx.update.message?.successful_payment);
-  ctx.reply(
-    JSON.stringify(ctx.update.message?.successful_payment.invoice_payload)
-  );
+  if (ctx.update.message?.successful_payment) {
+    const parsedData = JSON.parse(
+      ctx.update.message?.successful_payment.invoice_payload
+    );
+    ctx.reply(parsedData.type);
+  }
   ctx.refundStarPayment();
 });
 
